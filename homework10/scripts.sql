37194846dc0887f377a37d918d1ea670b7a4d533
M01eg <ggff6665@bk.ru>
2020-06-09 13:43:42 +0300

    Update scripts.sql

--- homework10/scripts.sql
+++ homework10/scripts.sql
@@ -6,50 +6,49 @@
 добавить необходимые индексы.
 */
 
+CREATE INDEX users_firstname_lastname_idx ON users(first_name, last_name);
+CREATE UNIQUE INDEX users_email_uq ON users(email);
+CREATE UNIQUE INDEX users_phone_uq ON users(phone);
+
+CREATE INDEX profiles_city_idx ON profiles(city);
+CREATE INDEX profiles_country_idx ON profiles(country);
+
+CREATE INDEX communities_name_idx ON communities(name);
+
+CREATE INDEX media_filename_idx ON media(filename);
+
+CREATE INDEX posts_head_idx ON posts(head);
+
+
+
+
+/*
+ Урок 10
+ Задание 2
+Задание на оконные функции
+
+Построить запрос, который будет выводить следующие столбцы:
+имя группы
+среднее количество пользователей в группах
+самый молодой пользователь в группе
+самый старший пользователь в группе
+общее количество пользователей в группе
+всего пользователей в системе
+отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
+*/
 
 SELECT
-  c.name,
-  FIRST_VALUE(c.) OVER c 
-FROM communities c
-JOIN (
-  SELECT
-    community_id,
-    COUNT(*)
-  FROM communities_users
-  GROUP BY community_id
-) community_population
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT
-  c.name,
-  NTH_VALUE(community_population.population, 13) OVER () AS average_population,
---  FIRST_VALUE(p.user_id) OVER (ORDER BY p.birthday DESC) AS youngest_user_id,
---  LAST_VALUE(p.user_id) OVER (ORDER BY p.birthday DESC) AS oldest_user_id
-  p.gender
-FROM
-  communities c
-  JOIN (
-    SELECT
-      c.id,
-      c.name,
-      cu.user_id,
-      count(cu.user_id) population
+    SUM(n_members) OVER () / (SELECT COUNT(*) FROM communities) AS n_avg_amount_of_members,
+    dummy.*
+FROM (
+    SELECT DISTINCT c.name,
+        FIRST_VALUE(p.user_id) OVER (PARTITION BY c.id ORDER BY p.birthday) AS youngest_user_id,
+        FIRST_VALUE(p.user_id) OVER (PARTITION BY c.id ORDER BY p.birthday DESC) AS oldest_user_id,
+        COUNT(p.user_id) OVER (PARTITION BY c.id) AS n_members,
+        COUNT(p.user_id) OVER () AS n_users_total,
+        CONCAT(100 * COUNT(p.user_id) OVER (PARTITION BY c.id) / COUNT(p.user_id) OVER (), '%') AS percent_of_total
     FROM communities c
-    LEFT JOIN communities_users cu ON c.id = cu.community_id
-    GROUP BY 1
-    ORDER BY population
-) community_population ON c.id = community_population.id
-  JOIN
-    profiles p ON community_population.user_id = p.user_id;
+        LEFT JOIN communities_users cu ON c.id = cu.community_id
+        LEFT JOIN profiles p ON cu.user_id = p.user_id
+    ORDER BY c.name
+) dummy;
